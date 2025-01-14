9aecc01654572f8a83a115d085810610ba9e1512
David <shurtleff123.ds@gmail.com>
2025-01-14 00:15:12 -0700

    initial commit

---
+++ db/connection.js
@@ -0,0 +1,27 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = "mongodb+srv://shurtleff123ds:hWWvucRDDZyoJXXa@dungeongodscluster0.47pbo.mongodb.net/?retryWrites=true&w=majority&appName=DungeonGodsCluster0" || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+    console.log(uri);
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
